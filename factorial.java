a91c6edc08d61969046687f765b5553e553fb994
AbhishekKmrSingh <91712759+AbhishekKmrSingh@users.noreply.github.com>
2022-10-03 17:02:03 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,18 @@
+
+// Java program to find factorial of given number
+class Test
+{
+ static int factorial(int n)
+ {
+  if (n == 0)
+  return 1;
+   
+  return n*factorial(n-1);
+ }
+  
+ public static void main(String[] args)
+ {
+  int num = 6;
+  System.out.println("Factorial of "+ num + " is " + factorial(6));
+ }
+}
